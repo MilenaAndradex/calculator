bcd01849be72f3da542f9396eb5244a12113163d
milena <milenaprado1020@gmail.com>
2023-04-11 17:00:02 -0300

    info

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from main_window import MainWindow
 from variables import WINDOW_ICON_PATH
 from PySide6.QtGui import QIcon
 from display import Display
+from info import Info
 
 
 
@@ -14,6 +15,10 @@ if __name__ == '__main__':
     #ícone
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
+
+    #info
+    info = Info('2.0 ^ 10.0 = 1024')
+    window.addToVlao(info)
 
     #display
     display = Display()
